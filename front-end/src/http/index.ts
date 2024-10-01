09f99a0703526822fa53ced748842cd584bc9e24
Alexander Ivanov <a.ivanov@krtech.ru>
2024-10-01 21:04:36 +0300

    fix base url

--- front-end/src/http/index.ts
+++ front-end/src/http/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://c6bc-188-191-29-81.ngrok-free.app';
+export const BASE_URL = 'https://api.olympus.ydns.eu/';
 
 const $api = axios.create({
     baseURL: BASE_URL,
