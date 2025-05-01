e2583edecbc2b8d7f230afae38883a3ce02636ca
Oksana Pylenok <o.pylenok@ideas-world.com>
2025-05-01 15:13:14 +0300

    impove styling in chips

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material";
+import { alpha, createTheme } from "@mui/material";
 import { ruRU } from "@mui/x-data-grid/locales";
 
 const theme = createTheme(
@@ -13,6 +13,32 @@ const theme = createTheme(
     },
     typography: {
       fontFamily: "'Manrope', sans-serif",
+    },
+    components: {
+      MuiChip: {
+        styleOverrides: {
+          root: ({ theme }) => ({
+            backgroundColor: alpha(theme.palette.primary.main, 0.1),
+            border: "none",
+            color: theme.palette.primary.main,
+            borderRadius: "6px",
+            transition: "opacity 0.5s",
+            "&:hover": {
+              backgroundColor: alpha(theme.palette.primary.main, 0.1),
+              color: theme.palette.primary.main,
+              opacity: 0.7,
+            },
+          }),
+          filled: ({ theme }) => ({
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.background.default,
+            "&:hover": {
+              backgroundColor: theme.palette.primary.main,
+              color: theme.palette.background.default,
+            },
+          }),
+        },
+      },
     },
   },
   ruRU
